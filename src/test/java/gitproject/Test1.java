359b687d389012f3ffde31592b3deaea813675c8
furrecs1907 <furkan_abay@hotmail.com>
2021-09-11 21:07:12 +0200

    Some changes

--- src/test/java/gitproject/Test1.java
+++ src/test/java/gitproject/Test1.java
@@ -3,5 +3,7 @@ package gitproject;
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("My first prj with Git");
+
+        System.out.println("I made some more changes to my project");
     }
 }
